b4966810c122a09f0ebf65cb6b3ca4ad3d5fad01
QuakeCrimeApp <diego.maldonado@uma.es>
2026-04-05 17:07:22 +0200

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,8 @@ if "barrios_disponibles" not in st.session_state:
         "Usera (Madrid)",
         "Ciutat Meridiana (Barcelona)",
         "La Mina (Sant Adrià del Besòs)",
-        "Palma Palmilla (Málaga)"
+        "Palma Palmilla (Málaga)",
+        "San Juan de Dios (Jerez de la Fra.)"
     ]
 
 # -----------------------------
